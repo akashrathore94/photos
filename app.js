c3c8c0d947a2f68a54a9664483880b7ff37b81c4
akashrathore94 <akashrathour2011@gmail.com>
2023-05-24 17:03:40 +0530

    js added

---
+++ app.js
@@ -0,0 +1,25 @@
+const auth = "YGu9OLifIjRpAkdMJmfgqpj4L8RuDFrmAzbrx9Zmc70RqivT32sZLYwj";
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const submitButton = document.querySelector(".submit-btn");
+let searchValue;
+
+async function curatedPhotos() {
+  const dataFetch = await fetch(
+    "https://api.pexels.com/v1/curated?per_page=1",
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    }
+  );
+
+  const data = await dataFetch.json();
+  console.log(data.photos[0].src);
+
+  document.querySelector(".abc").src = data.photos[0].src.large;
+}
+
+curatedPhotos();
